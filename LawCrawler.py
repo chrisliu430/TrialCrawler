8e8b05658f529cfc9dd19286e6bf381061937bf5
chrisliu430 <hearnechris430@gmail.com>
2020-04-16 08:57:41 +0800

    Fix some bug

--- LawCrawler.py
+++ LawCrawler.py
@@ -2,43 +2,54 @@ from bs4 import BeautifulSoup
 import requests
 
 class LawCrawler():
-    def __init__(self, link = "", number = 0, count = 0, durl = "", autoconfig = True, ferr = "./Error/err.log"):
+    def __init__(self, link="", fname="default", count=1, durl="", autoconfig=True, ferr="./Error/err.log", urlroot=""):
         self.link = link
-        self.number = number
+        self.fname = fname
         self.count = count
         self.durl = durl
         self.autoconfig = autoconfig
         self.ferr = open(ferr, "w")
+        self.urlroot = urlroot
+        self._get_html_parser()
 
     def _get_html_parser(self):
         rs = requests.get(self.link)
         self.parser = BeautifulSoup(rs.text, "html.parser")
 
-    def write_link(self, fname="./Data/Default.txt"):
+    def find_download_url(self, elink=""):
+        rs = requests.get(elink)
+        parser = BeautifulSoup(rs.text, "html.parser")
+        targets = parser.find(class_='attachment').find(class_='filename').find_all('a')
+        for target in targets:
+            self.durl = self.urlroot + target['href']
+            self.download_file()
+
+    def write_download_url(self, wurl="", fwrite="./Data/Default.txt"):
         pass
 
-    def search_tag(self, target = ""):
-        pass
-
-    def search_all_tags(self, target = ""):
+    def change_link(self, link = ""):
+        self.link = link
         self._get_html_parser()
-        print(self.link)
-        ftarget = self.parser.find(target)
-        print(ftarget.encode('utf-8'))
-        # for aftarget in ftarget:
-        #     print(aftarget.encode('utf-8'))
 
-    def download_file(self, dformat = ""):
+    def get_count(self):
+        return self.count // 2
+
+    def find_externallink(self):
+        elinks = self.parser.find(class_='list').find('table').find_all('a')
+        for elink in elinks:
+            self.find_download_url(elink=self.urlroot + elink['href'])
+
+    def download_file(self, dformat=".pdf"):
         self.durl = self.durl.rstrip('\n')
         rs = requests.get(self.durl)
         if (rs.headers['content-type'] != "text/html; Charset=utf-8"):
-            with open('./Patent' + str(self.count) + dformat, 'wb') as writef:
+            with open('./' + self.fname + str(self.count) + dformat, 'wb') as writef:
                 writef.write(rs.content)
                 self.count += 1
         else:
             self.ferr.write(self.durl + "\n")
 
-    def load_download_target(self, dformat = "", fread = ""):
+    def load_download_target(self, dformat="", fread="./Data/Default.txt"):
         if (fread != ""):
             fread = open(fread, "r")
             for durl in fread:
--- Setup.py
+++ Setup.py
@@ -1,33 +1,18 @@
 import LawCrawler
 
-def entrance():
-    lc = LawCrawler.LawCrawler("https://www1.tipo.gov.tw/lp.asp?CtNode=7199&CtUnit=3259&BaseDSD=7&mp=1")
-    lc.search_all_tags('a')
+def entrance(target):
+    find = 0
+    # Patent Url
+    tlink = "https://www1.tipo.gov.tw/lp.asp?CtNode=7199&CtUnit=3259&BaseDSD=7&mp=1"
+    clink = tlink
+    lc = LawCrawler.LawCrawler(link = tlink, fname = "Patent", urlroot = "https://www1.tipo.gov.tw/")
+    while(find < target):
+        lc.find_externallink()
+        find = lc.get_count()
+        if (find >= 10):
+            clink = tlink + "&nowPage=" + str(lc.get_count() // 10 + 1) + "&pagesize=10"
+        lc.change_link(clink)
+    lc.default_exit()
     
 if __name__ == "__main__":
-    entrance()
-
-# def writeLink(sUrl):
-#     nUrl = "https://www1.tipo.gov.tw/" + sUrl
-#     nRs = requests.get(nUrl)
-#     nSoup = BeautifulSoup(nRs.text, 'html.parser')
-#     nTags = nSoup.find(class_='attachment').find(class_='filename').find_all('a')
-#     for nTag in nTags:
-#         f.write("https://www1.tipo.gov.tw/" + nTag['href'] + "\n")
-
-# def twoHundred():
-#     global total
-#     while (total < 160):
-#         url = 'https://www1.tipo.gov.tw/lp.asp?CtNode=7199&CtUnit=3259&BaseDSD=7&mp=1'
-#         if (total >= 10):
-#             nPage = total // 10 + 1
-#             url = url + "&nowPage=" + str(nPage) + "&pagesize=10"
-#         rs = requests.get(url)
-#         soup = BeautifulSoup(rs.text, 'html.parser')
-#         tags = soup.find(class_='list').find('table').find_all('a')
-#         for tag in tags:
-#             writeLink(tag['href'])
-#             total += 1
-
-# if __name__ == "__main__":
-#     twoHundred()
+    entrance(target = 10)
